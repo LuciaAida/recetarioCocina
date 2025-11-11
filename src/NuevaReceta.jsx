d4fcaf297f0bb628d5063dc57d52f158cbe5e0ef
LuciaAida <luciapa799@gmail.com>
2025-11-11 19:18:23 +0100

    Inicio form para nuevas recetas

--- src/NuevaReceta.jsx
+++ src/NuevaReceta.jsx
@@ -1,11 +1,37 @@
 import './index.css'
+import { useState } from 'react';
 
-function NuevaReceta(){
-    return(
-        <div>
-            <h1>Hiaja</h1>
-        </div>
-    )
+function nuevaReceta(){
+    const [receta, setReceta] = useState({titulo:"", descripcion:""});
+
+
+//nuevo objeto, copia lo anterior y lo reemplaza
+function actualizarCampo( campo, valor ){
+    setReceta({
+        ...receta,
+        [campo]:valor
+    })
 }
 
-export default NuevaReceta
+
+    return(
+        <div>
+            <h1>Nueva receta</h1>
+            <h3>Título</h3>
+            <input 
+                type="text"
+                value={receta.titulo} //valor actual
+                onChange={(e) => actualizarCampo("titulo", e.target.value)}/>
+
+            <h3>Descripción</h3>
+            <textarea 
+                value={receta.descripcion}
+                onChange={(e) => actualizarCampo("descripcion", e.target.value)}/>
+
+            {/* <button onClick={guardarReceta}>Guardar receta</button> */}
+        </div>
+    );
+}
+
+
+export default nuevaReceta
